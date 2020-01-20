4329917f291fcc6d112aeb9527b244c1aa024544
Trinity Terry <trinitycterry@gmail.com>
2020-01-20 15:24:38 -0600

    finished excercise

--- script.js
+++ script.js
@@ -10,23 +10,30 @@ const DragDropManager = Object.create(null, {
                 };
             });
 
-
             const targets = document.querySelectorAll(".target");
-
+            
             targets.forEach(target => {
                 // Dragover not supported by default. Turn that off.
                 target.ondragover = e => e.preventDefault();
-
+                
                 target.ondrop = e => {
+                    
                     // Enabled dropping on targets
                     e.preventDefault();
 
                     // Determine what's being dropped
                     const data = e.dataTransfer.getData("text");
-
+                    
                     // Append card to target component as child
                     // TODO: This should only happen if the target has no children nodes
-                    e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`));
+                    // e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`));
+                    if (e.target.classList[0] == "stage"){
+                        target.appendChild(document.querySelector(`.${data.split(" ")[1]}`));
+                    }else if (target.classList[1] == "grid" && (e.target.classList[0] != "stage")){
+                        e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`));
+                    } else if (target.children.length < 1 ){
+                        e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`));
+                    }
                 };
             });
         }
